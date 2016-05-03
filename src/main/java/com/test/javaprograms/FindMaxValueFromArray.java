2869b53ea440e7ec72950d55ca7e041e3838e20f
Chandan <Chandan.Mandal@ATMECS.CORP>
2016-05-03 17:05:08 +0530

    Added code for Max and Min program

--- src/main/java/com/test/javaprograms/FindMaxValueFromArray.java
+++ src/main/java/com/test/javaprograms/FindMaxValueFromArray.java
@@ -3,8 +3,16 @@ package com.test.javaprograms;
 public class FindMaxValueFromArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int array[] = {10,33,2,97,500};
+		int maxValue = array[0];
+		for(int i = 1;i<array.length;i++){
 
+			if(maxValue<array[i]){
+
+				maxValue = array[i];
+			}
+		}
+		System.out.println("Max vaue present in array is: "+maxValue);
 	}
 
 }
--- src/main/java/com/test/javaprograms/FindMinValueFromArray.java
+++ src/main/java/com/test/javaprograms/FindMinValueFromArray.java
@@ -3,8 +3,17 @@ package com.test.javaprograms;
 public class FindMinValueFromArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int array[] = {10,33,2,97,500};
+		int minValue = array[0];
+		for(int i = 1;i<array.length;i++){
 
+			if(minValue>array[i]){
+
+				minValue = array[i];
+			}
+		}
+		System.out.println("Min vaue present in array is: "+minValue);
 	}
-
 }
+
+
